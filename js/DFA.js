011c9b0a1b22ba1008fb4f723b69fa2d170c58e8
Jefoliva <jefodesigner@gmail.com>
2019-08-29 10:56:55 -0600

    Agregar retorno de mensaje de error al leer un caracter invalido

--- js/DFA.js
+++ js/DFA.js
@@ -3,22 +3,27 @@ class DFA {
         this.action = [];
         this.next = [];
 
-        let n = input.shift();
-        let alphabet = input.shift();
+        let n = input.pop();
+        let alphabet = input.pop();
 
         for (let state = 0; state < n; state++) {
-            this.action[state] = input.shift();
+            this.action[state] = input.pop();
             this.next[state] = new Map();
             for (let i = 0; i < alphabet.length; i++)
-                this.next[state].set(alphabet[i], input.shift());
+                this.next[state].set(alphabet[i], input.pop());
         }
     }
 
     simulate(input) {
         let state = 0;
         for (let i = 0; i < input.length; i++) {
-            state = this.next[state].get(input[i]);
+
+            if(this.next[state].has(input[i]))
+                state = this.next[state].get(input[i]);
+            else
+                return "Error: Caracter Invalido";
         }
-        return this.action[state];
+
+        return this.action[state] === "SI" ? "VALIDO" : "INVALIDO";
     }
 }
